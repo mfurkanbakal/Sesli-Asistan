140140626219bb62c691cc6fa84576346480bae8
mfurkanbakal <81307238+mfurkanbakal@users.noreply.github.com>
2021-03-24 16:43:27 +0300

    ilk gonderme

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Sesli_Asistan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(105, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(68, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 283);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 279);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Voice asistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+namespace Sesli_Asistan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PromptBuilder okuyucu = new PromptBuilder();
+        SpeechSynthesizer konuşma_sentez = new SpeechSynthesizer();
+        SpeechRecognitionEngine konuşma_motoru = new SpeechRecognitionEngine();
+        bool listening = false;
+
+       
+        private void Konuşma_motoru_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string alınan_ses = e.Result.Text;
+            
+            
+            if(alınan_ses=="asistan")
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am listening");
+                konuşma_sentez.Speak(okuyucu);
+                listening = true;
+
+                label1.BackColor = Color.Red;
+             
+            }
+
+            if (alınan_ses == "stop listening")
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Okey I stop listening");
+                konuşma_sentez.Speak(okuyucu);
+                listening = false;
+            }
+           
+            if (alınan_ses =="google" && listening==true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("C:/Program Files/Google/Chrome/Application/chrome.exe");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the google chrome");
+                konuşma_sentez.Speak(okuyucu);
+            }
+            
+            if(alınan_ses == "open youtube" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://www.youtube.com/");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the youtube");
+                konuşma_sentez.Speak(okuyucu);
+
+            }
+            if (alınan_ses == "open moodle" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://moodle.atilim.edu.tr/");
+  
+
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the moodle");
+                konuşma_sentez.Speak(okuyucu);
+
+            }
+
+            if (alınan_ses == "open gmail" && listening == true)
+            {  okuyucu.ClearContent();
+                 okuyucu.AppendText("Tamam");
+                   konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the gmail");
+                konuşma_sentez.Speak(okuyucu);
+
+            }
+
+            if (alınan_ses == "open hotmail" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://outlook.live.com/owa/");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the hotmail");
+                konuşma_sentez.Speak(okuyucu);
+
+            }
+           
+            if (alınan_ses== "exit" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Bye Bye");
+                konuşma_sentez.Speak(okuyucu);
+                Application.Exit();
+            }
+
+            if (alınan_ses == "open dev c" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("C:/Program Files (x86)/Dev-Cpp/devcpp.exe");
+               
+
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the dev c");
+                konuşma_sentez.Speak(okuyucu);
+            }
+            if (alınan_ses == "open discord" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("C:/Users/MFB44/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Discord Inc/Discord.lnk");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the discord");
+                konuşma_sentez.Speak(okuyucu);
+                
+
+            }
+            if (alınan_ses == "open translate" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://translate.google.com/");
+               
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the google translate");
+                konuşma_sentez.Speak(okuyucu);
+
+            }
+            if (alınan_ses == "today weather" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Today the weather is like this");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("https://www.google.com/search?q=hava+durumu&oq=hava+&aqs=chrome.0.69i59i131i433j0i131i433j69i57j0i131i433l2j69i60l3.2825j1j7&sourceid=chrome&ie=UTF-8");
+
+            }
+            if (alınan_ses == "open blender" && listening == true)
+            {
+                okuyucu.ClearContent();
+                okuyucu.AppendText("Tamam");
+                konuşma_sentez.Speak(okuyucu);
+                System.Diagnostics.Process.Start("C:/Program Files/Blender Foundation/Blender 2.92/blender.exe");
+                okuyucu.ClearContent();
+                okuyucu.AppendText("I am opening the blender");
+                konuşma_sentez.Speak(okuyucu);                
+            }
+
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+            okuyucu.ClearContent();
+            okuyucu.AppendText("Hello I am your assistant if you want to me something before you should say asistan");
+            konuşma_sentez.Speak(okuyucu);
+            konuşma_motoru.SetInputToDefaultAudioDevice();
+            Choices işlemler = new Choices("open blender","asistan","today weather", "open translate","open discord", "exit", "open gmail", "open hotmail", "open dev c",  "open moodle","google", "open youtube", "stop listening");
+            GrammarBuilder gramer_yapıcı = new GrammarBuilder(işlemler);
+            gramer_yapıcı.Culture = System.Globalization.CultureInfo.GetCultureInfoByIetfLanguageTag("en-US");
+
+            Grammar gramer = new Grammar(gramer_yapıcı);
+            konuşma_motoru.LoadGrammar(gramer);
+            konuşma_motoru.SpeechRecognized += Konuşma_motoru_SpeechRecognized; ;
+            konuşma_motoru.RecognizeAsync(RecognizeMode.Multiple);
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Red;
+        }
+    }
+}
